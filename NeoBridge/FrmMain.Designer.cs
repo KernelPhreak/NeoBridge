3544dcaaf721528204a2430f8a59fc5f6f989100
KernelPhreak <itsdaddius@gmail.com>
2026-01-09 14:17:49 -0600

    Start of UI

--- NeoBridge/FrmMain.Designer.cs
+++ NeoBridge/FrmMain.Designer.cs
@@ -30,15 +30,28 @@
         {
             statusStrip1 = new StatusStrip();
             LblStatus = new ToolStripStatusLabel();
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            comboBox3 = new ComboBox();
+            button1 = new Button();
+            groupBox2 = new GroupBox();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            button2 = new Button();
+            groupBox3 = new GroupBox();
             statusStrip1.SuspendLayout();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
             SuspendLayout();
             // 
             // statusStrip1
             // 
             statusStrip1.Items.AddRange(new ToolStripItem[] { LblStatus });
-            statusStrip1.Location = new Point(0, 362);
+            statusStrip1.Location = new Point(0, 460);
             statusStrip1.Name = "statusStrip1";
-            statusStrip1.Size = new Size(673, 22);
+            statusStrip1.Size = new Size(701, 22);
             statusStrip1.SizingGrip = false;
             statusStrip1.TabIndex = 0;
             statusStrip1.Text = "statusStrip1";
@@ -49,11 +62,116 @@
             LblStatus.Size = new Size(88, 17);
             LblStatus.Text = "Not Connected";
             // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(comboBox3);
+            groupBox1.Controls.Add(comboBox2);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(677, 82);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Camera Connection";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(21, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(45, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Device:";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(72, 27);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(169, 23);
+            comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(247, 27);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(164, 23);
+            comboBox2.TabIndex = 2;
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(417, 27);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(164, 23);
+            comboBox3.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(587, 27);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 4;
+            button1.Text = "Connect";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(button2);
+            groupBox2.Controls.Add(textBox1);
+            groupBox2.Controls.Add(label2);
+            groupBox2.Location = new Point(12, 106);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(677, 153);
+            groupBox2.TabIndex = 2;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Auto Transfer Settings";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(21, 32);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Save To:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(77, 29);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(334, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(417, 29);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 5;
+            button2.Text = "Browse";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Location = new Point(12, 271);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(677, 184);
+            groupBox3.TabIndex = 3;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Transfer Log";
+            // 
             // FrmMain
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(673, 384);
+            ClientSize = new Size(701, 482);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
             Controls.Add(statusStrip1);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             MaximizeBox = false;
@@ -62,6 +180,10 @@
             Text = "NeoBridge";
             statusStrip1.ResumeLayout(false);
             statusStrip1.PerformLayout();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -70,5 +192,16 @@
 
         private StatusStrip statusStrip1;
         private ToolStripStatusLabel LblStatus;
+        private GroupBox groupBox1;
+        private Button button1;
+        private ComboBox comboBox3;
+        private ComboBox comboBox2;
+        private ComboBox comboBox1;
+        private Label label1;
+        private GroupBox groupBox2;
+        private TextBox textBox1;
+        private Label label2;
+        private Button button2;
+        private GroupBox groupBox3;
     }
 }
