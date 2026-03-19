0eb7b9e0b226f3f0b9ef6a7dd74cdb6a04f47314
KernelPhreak <artw1982@gmail.com>
2026-03-18 21:29:12 -0500

    working on project models

--- NeoBridge/Models/ProjectModel.cs
+++ NeoBridge/Models/ProjectModel.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace NeoBridge.Models;
 
-namespace NeoBridge.Models
+internal class ProjectModel
 {
-    internal class ProjectModel
-    {
-        public string ProjectName { get; set; } = string.Empty;
-        public string ProjectPath { get; set; } = string.Empty;
-        public string ProjectGuid { get; set; } = string.Empty;
-        public string ExportPath { get; set; } = string.Empty;
-        public bool ShowOnImport { get; set; } = true;
-        public bool RenameOnExport { get; set; } = true;
-        public string RenamePattern { get; set; } = "{original_name}_{timestamp}";
-    }
+    public string ProjectName { get; set; } = string.Empty;
+    public string ProjectPath { get; set; } = string.Empty;
+    public string ProjectGuid { get; set; } = string.Empty;
+    public string ExportPath { get; set; } = string.Empty;
+    public bool ShowOnImport { get; set; } = true;
+    public bool RenameOnExport { get; set; } = true;
+    public string RenamePattern { get; set; } = "{original_name}_{timestamp}";
 }
