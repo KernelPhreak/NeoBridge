bd107b1dc1340e91103c3740564d0746f1a55abc
KernelPhreak <artw1982@gmail.com>
2026-03-18 21:24:52 -0500

    lil

--- NeoBridge/Models/ProjectModel.cs
+++ NeoBridge/Models/ProjectModel.cs
@@ -6,5 +6,12 @@ namespace NeoBridge.Models
 {
     internal class ProjectModel
     {
+        public string ProjectName { get; set; } = string.Empty;
+        public string ProjectPath { get; set; } = string.Empty;
+        public string ProjectGuid { get; set; } = string.Empty;
+        public string ExportPath { get; set; } = string.Empty;
+        public bool ShowOnImport { get; set; } = true;
+        public bool RenameOnExport { get; set; } = true;
+        public string RenamePattern { get; set; } = "{original_name}_{timestamp}";
     }
 }
